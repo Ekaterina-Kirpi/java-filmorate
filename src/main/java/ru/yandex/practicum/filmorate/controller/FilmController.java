8909942aa68f7e1f7fd73299944aaf38bcc72a48
Kirpi <kirpicheva89@yandex.ru>
2023-04-18 13:59:55 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -23,7 +23,7 @@ public class FilmController {
     private final Map<Integer, Film> filmsMap = new HashMap<>();
     private static int filmId = 1;
     private final int maxDescriptionLength = 200;
-    private  final LocalDate minReleaseDate = LocalDate.of(1895, 12, 28);
+    private final LocalDate minReleaseDate = LocalDate.of(1895, 12, 28);
 
     @GetMapping
     public List<Film> getFilms() {
@@ -45,27 +45,28 @@ public class FilmController {
     public Film update(@Valid @RequestBody Film film) {
         int id = film.getId();
         if (!filmsMap.containsKey(id)) {
-            throw new ValidationException(HttpStatus.NOT_FOUND,"Фильм не найден");
+            throw new ValidationException(HttpStatus.NOT_FOUND, "Фильм не найден");
         } else {
             filmsMap.replace(id, film);
             log.info("Фильм обновлен" + film);
         }
         return film;
     }
+
     @ResponseBody
     public void validateFilm(Film film) {
         if (filmsMap.containsKey(film.getId())) {
             log.warn("Такой фильм уже есть в коллекции");
-            throw new ValidationException(HttpStatus.BAD_REQUEST,"Такой фильм уже есть в коллекции");
+            throw new ValidationException(HttpStatus.BAD_REQUEST, "Такой фильм уже есть в коллекции");
         } else if (film.getReleaseDate().isBefore(minReleaseDate)) {
             log.warn("Неверно указана дата выпуска");
-            throw new ValidationException(HttpStatus.BAD_REQUEST,"Неверно указана дата выпуска");
+            throw new ValidationException(HttpStatus.BAD_REQUEST, "Неверно указана дата выпуска");
         } else if (film.getDescription().length() > maxDescriptionLength) {
             log.warn("Максимальная длина описания - 200 символов");
-            throw new ValidationException(HttpStatus.BAD_REQUEST,"Максимальная длина описания - 200 символов");
+            throw new ValidationException(HttpStatus.BAD_REQUEST, "Максимальная длина описания - 200 символов");
         } else if (film.getDuration() < 0) {
             log.warn("Продолжительность фильма должна быть положительной");
-            throw new ValidationException(HttpStatus.BAD_REQUEST,"Продолжительность фильма должна быть положительной");
+            throw new ValidationException(HttpStatus.BAD_REQUEST, "Продолжительность фильма должна быть положительной");
         }
     }
 
