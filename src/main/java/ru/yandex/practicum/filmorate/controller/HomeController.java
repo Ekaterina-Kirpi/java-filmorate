cdbdda4cc33ecb1305e32b1032cd00f0ebfd9436
Kirpi <kirpicheva89@yandex.ru>
2023-04-18 14:02:16 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/controller/HomeController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class HomeController {
     @GetMapping
-    public String homePage(){
+    public String homePage() {
         return "Добро пожаловать в Filmorate";
     }
 }
