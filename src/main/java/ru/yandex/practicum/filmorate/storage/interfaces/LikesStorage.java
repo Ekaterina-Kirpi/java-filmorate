d3ad4206ca86a90ceabba21b7bbd1cf8c1057c69
Kirpi <kirpicheva89@yandex.ru>
2023-05-24 01:29:57 +0300

    Добавлена БД

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/LikesStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/LikesStorage.java
@@ -5,7 +5,9 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface LikesStorage {
-    void  addLike(long filmId, long userId);
+    void addLike(long filmId, long userId);
+
     void deleteLike(long filmId, long userId);
+
     List<Film> getTopLikedFilms(long count);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/MpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/MpaStorage.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface MpaStorage {
     List<Mpa> getAllRatingMpa();
+
     Mpa getMpaById(int id);
 
 }
